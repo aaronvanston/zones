f2ab2edd9ab78dd15315e622ac56a332a8d7a4dc
Aaron Vanston <aaron.vanston@rea-group.com>
2017-08-06 15:21:41 +1000

    feat: add modal to Add new Zone button

--- src/components/AddNewZone/AddNewZone.js
+++ src/components/AddNewZone/AddNewZone.js
@@ -1,17 +1,48 @@
 import React from 'react';
 
-import { CardContainer } from '../';
+import { CardContainer, Modal } from '../';
 
 import styles from './AddNewZone.css';
 
-const AddNewZone = () => (
-  <CardContainer>
-    <button className={styles.card}>
-      <span className={styles.heading}>Add new</span>
-      <span className={styles.sub}>Zone</span>
-    </button>
-  </CardContainer>
-);
+class AddNewZone extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      modalIsOpen: false,
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({ modalIsOpen: true });
+  }
+
+  closeModal() {
+    this.setState({ modalIsOpen: false });
+  }
+
+  render() {
+    return (
+      <CardContainer>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onRequestClose={this.closeModal}
+          contentLabel="Add new zone"
+        >
+          Add New Zone
+        </Modal>
+
+        <button className={styles.card} onClick={this.openModal}>
+          <span className={styles.heading}>Add new</span>
+          <span className={styles.sub}>Zone</span>
+        </button>
+      </CardContainer>
+    );
+  }
+}
 
 
 export default AddNewZone;
