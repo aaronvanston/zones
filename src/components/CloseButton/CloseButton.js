d3112d17339cbc280d39621c865b3401f41710d3
Aaron Vanston <aaron.vanston@rea-group.com>
2017-12-04 14:37:57 +1100

    feat: add locationInput with react autosuggest

---
+++ src/components/CloseButton/CloseButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styles from './CloseButton.css';
+
+const CloseButton = () => (
+  <button className={styles.close}>
+    Close
+  </button>
+);
+
+
+export default CloseButton;
---
+++ src/components/LocationInput/LocationInput.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import Autosuggest from 'react-autosuggest';
+
+import { CloseButton } from '../';
+
+import styles from './LocationInput.css';
+
+const locations = [
+  {
+    location: 'Melbourne',
+    gmt: +11,
+  },
+  {
+    location: 'Meltest',
+    gmt: +12,
+  },
+  {
+    location: 'Sydney',
+    gmt: +11,
+  },
+];
+
+const getSuggestions = (value) => {
+  const inputValue = value.trim().toLowerCase();
+  const inputLength = inputValue.length;
+
+  return inputLength === 0 ? [] : locations.filter(loc =>
+    loc.location.toLowerCase().slice(0, inputLength) === inputValue,
+  );
+};
+
+const getSuggestionValue = suggestion => suggestion.location;
+
+const renderSuggestion = suggestion => (
+  <div>
+    <div>{suggestion.location}</div>
+    <div>{suggestion.gmt}</div>
+  </div>
+);
+
+class LocationInput extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: '',
+      suggestions: [],
+    };
+  }
+
+  onChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue,
+    });
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: [],
+    });
+  };
+
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: getSuggestions(value),
+    });
+  };
+
+  render() {
+    const { value, suggestions } = this.state;
+
+    const inputProps = {
+      placeholder: 'Enter a location',
+      value,
+      onChange: this.onChange,
+    };
+
+    return (
+      <div className={styles.wrapper}>
+        <Autosuggest
+          theme={styles}
+          suggestions={suggestions}
+          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+          getSuggestionValue={getSuggestionValue}
+          renderSuggestion={renderSuggestion}
+          inputProps={inputProps}
+          alwaysRenderSuggestions
+        />
+        <CloseButton />
+      </div>
+    );
+  }
+}
+
+export default LocationInput;
